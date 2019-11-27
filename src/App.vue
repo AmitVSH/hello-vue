ebf1c7b6acc1c160af9d9001a8307c876a5e4c2f
Amit Kadam <amit.kadam@vshsolutions.com>
2019-11-27 09:52:21 +0530

    Structure cleaned

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,32 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/welcome">Welcome</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+body {
+margin:0px;
+padding:0px;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  font-family: 'Montserrat', sans-serif;
 }
 
 #nav {
   padding: 30px;
+  background-color: #c3ebcf;
 
   a {
     font-weight: bold;
