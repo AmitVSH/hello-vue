b6a45e5510c952e10e9147d7410339d1da81fbcb
Amit Kadam <amit.kadam@vshsolutions.com>
2019-11-27 10:37:55 +0530

    Navbar Styling Added

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
     <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link> |
-        <router-link to="/welcome">Welcome</router-link>
+        <router-link class="nav_link" to="/">Home</router-link>
+        <router-link class="nav_link" to="/about">About</router-link>
+        <router-link class="nav_link" to="/welcome">Welcome</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+@import url('https://fonts.googleapis.com/css?family=Prompt&display=swap');
 body {
 margin:0px;
 padding:0px;
@@ -21,7 +21,7 @@ padding:0px;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  font-family: 'Montserrat', sans-serif;
+  font-family: 'Prompt', sans-serif;
 }
 
 #nav {
@@ -36,5 +36,13 @@ padding:0px;
       color: #42b983;
     }
   }
+}
+
+.nav_link {
+  background-color: #fff;
+  padding: 5px 20px;
+  border-radius: 5px;
+  margin: 10px;
+  text-decoration: none;
 }
 </style>
