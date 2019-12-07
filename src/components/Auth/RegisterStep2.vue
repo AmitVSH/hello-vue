a9f0a378134e791087480766046e7bf6c92d91dc
Amit Kadam <amit.kadam@vshsolutions.com>
2019-12-07 11:39:27 +0530

    mixin added in registration step2

--- src/components/Auth/RegisterStep2.vue
+++ src/components/Auth/RegisterStep2.vue
@@ -38,26 +38,19 @@
 import { mapState } from "vuex";
 import InputText from "../Library/InputText";
 import InputButton from "../Library/InputButton";
+
+import { main } from "../../mixins/main";
 export default {
   name: "RegisterStep2",
   components: {
     InputText,
     InputButton
   },
+  mixins: [main],
   computed: {
     ...mapState({
       user: state => state.user
     }),
-
-    registrationProgress: function() {
-      let progress = 0;
-      if(this.user.email.length > 0) progress += 25;
-      if(this.user.mobile.length > 0) progress += 25;
-      if(this.user.name.length > 0) progress += 25;
-      if(this.user.address.length > 0) progress += 25;
-
-      return progress+"% completed...";
-    }
   }
 }
 </script>
