76bb90c3d2f8efe623c32a14266417b58e26311e
Amit Kadam <amit.kadam@vshsolutions.com>
2019-12-07 11:29:16 +0530

    We need to check form progress, registrationProgress computed property added

--- src/components/Auth/RegisterStep1.vue
+++ src/components/Auth/RegisterStep1.vue
@@ -1,6 +1,9 @@
 <template lang="html">
   <div class="register__container">
     <div class="register__wrapper">
+      <div class="progress__wrapper">
+        <h3>{{registrationProgress}}</h3>
+      </div>
       <InputText>
         <!-- component body is passed with slots -->
         <label for="mobile">Mobile No:</label>
@@ -50,7 +53,17 @@ export default {
   computed: {
     ...mapState({
       user: state => state.user
-    })
+    }),
+
+    registrationProgress: function() {
+      let progress = 0;
+      if(this.user.email.length > 0) progress += 25;
+      if(this.user.mobile.length > 0) progress += 25;
+      if(this.user.name.length > 0) progress += 25;
+      if(this.user.address.length > 0) progress += 25;
+
+      return progress+"% completed...";
+    }
   }
 }
 </script>
